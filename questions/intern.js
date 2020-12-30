33fae248ee0c021c072f0c80888ff839064b6736
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-30 15:31:18 -0500

    added validation

--- questions/intern.js
+++ questions/intern.js
@@ -2,22 +2,46 @@ const internQuestions = [
     {
         type: "input",
         name: "internName",
-        message: "What is the intern's name?"
+        message: "What is the intern's name?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "Name field must be filled out"
+        }
     },
     {
         type: "input",
         name: "internEmail",
-        message: "What is the intern's email?"
+        message: "What is the intern's email?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "Email field must be filled out"
+        }
     },
     {
         type: "input",
         name: "internId",
-        message: "What is the intern's ID number?"
+        message: "What is the intern's ID number?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "ID field must be filled out"
+        }
     },
     {
         type: "input",
         name: "internSchool",
-        message: "What is the intern's school?"
+        message: "What is the intern's school?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "School field must be filled out"
+        }
     },
 ]
 
