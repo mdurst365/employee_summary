86ca90c90c52d03f71073981ba26f0f969d2961b
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-30 12:36:13 -0500

    Create engineer.js

---
+++ questions/engineer.js
@@ -0,0 +1,24 @@
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "engineerName",
+        message: "What is the Engineer's name?"
+    },
+    {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the Engineer's email?"
+    },
+    {
+        type: "input",
+        name: "engineerId",
+        message: "What is the Engineer's ID number?"
+    },
+    {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the Engineer's github?"
+    },
+]
+
+module.exports = engineerQuestions;
