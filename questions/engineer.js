b4bb2de46e5494aefa8d78e589ec7869e7f32104
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-30 15:33:11 -0500

    added validation

--- questions/engineer.js
+++ questions/engineer.js
@@ -2,22 +2,46 @@ const engineerQuestions = [
     {
         type: "input",
         name: "engineerName",
-        message: "What is the Engineer's name?"
+        message: "What is the Engineer's name?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "Name field must be filled out"
+        }
     },
     {
         type: "input",
         name: "engineerEmail",
-        message: "What is the Engineer's email?"
+        message: "What is the Engineer's email?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "Email field must be filled out"
+        }
     },
     {
         type: "input",
         name: "engineerId",
-        message: "What is the Engineer's ID number?"
+        message: "What is the Engineer's ID number?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "ID field must be filled out"
+        }
     },
     {
         type: "input",
         name: "engineerGithub",
-        message: "What is the Engineer's github?"
+        message: "What is the Engineer's github?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "Github field must be filled out"
+        }
     },
 ]
 
