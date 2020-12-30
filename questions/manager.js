071faabfa9d492d90e252c07bd7adb52f512998e
mdurst365 <71846329+mdurst365@users.noreply.github.com>
2020-12-30 15:29:56 -0500

    added validation

--- questions/manager.js
+++ questions/manager.js
@@ -7,23 +7,41 @@ const managerQuestions = [
             if(answer !== "") {
                 return true
             }
-            return "Please do not leave any blank fields"
+            return "Name field must be filled out"
         }
     },
     {
         type: "input",
         name: "managerEmail",
-        message: "What is the manager's email?"
+        message: "What is the manager's email?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            } 
+            return "Email field must be filled out"
+        }
     },
     {
         type: "input",
         name: "managerId",
-        message: "What is the manager's ID number?"
+        message: "What is the manager's ID number?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "ID field must be filled out"
+        }
     },
     {
         type: "input",
         name: "managerOffice",
-        message: "What is the manager's office number?"
+        message: "What is the manager's office number?",
+        validate: (answer) => {
+            if(answer !== "") {
+                return true
+            }
+            return "ID field must be filled out"
+        }
     },
 ]
 
